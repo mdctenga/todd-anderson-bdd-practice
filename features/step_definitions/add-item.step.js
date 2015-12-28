fe994dab0a98ad93ae399ce4431c063b3e4f812d
mdctenga <mdctenga@gmail.com>
2015-12-28 11:09:33 -1000

    Add generated step_defintion snippets to

---
+++ features/step_definitions/add-item.step.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function function_name (argument) {
+
+  this.Given(/^I have an empty grocery list$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^I add an item to the list$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^the grocery list contains a single item$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^I can acces that item from the grocery list$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+};
